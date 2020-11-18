a750519b37c2ef23c8a9b3c0582c070e2ce5c39f
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-11-18 22:33:37 +0530

    Added : Start and Winning position

--- com/snake/Snake_And_Ladder.java
+++ com/snake/Snake_And_Ladder.java
@@ -1,9 +1,12 @@
 package com.snake;
 
 public class Snake_And_Ladder {
-
+public static final int startPosition=0;
+public static final int WinningPosition=100;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and ladder game");
+		System.out.println("Start position is " + startPosition);
+		System.out.println("Winning position is " + WinningPosition);
 	}
 
 }
