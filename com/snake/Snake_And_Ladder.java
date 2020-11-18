1bb6ddc39608db7ef5930952a57596b36ee91434
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-11-19 01:25:22 +0530

    Added : Exact winning position 100

--- com/snake/Snake_And_Ladder.java
+++ com/snake/Snake_And_Ladder.java
@@ -28,28 +28,32 @@ public class Snake_And_Ladder {
 		
 		while(newPosition<WinningPosition) {
 			int playValue=optionForPlay();
-		
+			int diceRollValue=throwDie();
+			System.out.println("~~~ Dice rolled value: "+diceRollValue);
 			if(playValue==0) {
 				System.out.println("No play");
 				newPosition = newPosition;
 			}
 			else if(playValue==1) {
 				System.out.println("got ladder");
-				newPosition = newPosition + throwDie();
+				newPosition = newPosition + diceRollValue;
 				if(newPosition>100) {
-					newPosition=100;
-					System.out.println("congratulations player reached to winning position");
+					newPosition=newPosition-diceRollValue;
+					
 				}
 			}
 			else if(playValue==2) {
 				System.out.println("snake attack");
-				newPosition = newPosition - throwDie();	
+				newPosition = newPosition - diceRollValue;	
 				if(newPosition<0) {
 					newPosition=0;
 				}
 			}
 		
 			System.out.println("new position is : " +newPosition);
+			if(newPosition==100) {
+				System.out.println("congratulations player reached to winning position");
+			}
 		}
 
 	}	
